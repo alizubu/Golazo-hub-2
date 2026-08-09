ee22866aeca10da6ded7bf33469fb4001cccf5f9
alizubu <azzubair1470@gmail.com>
2026-08-09 15:05:39 +0600

    Fix upload server action to use NEXT_PUBLIC_CLOUDINARY_API_KEY

--- app/actions/upload.js
+++ app/actions/upload.js
@@ -5,7 +5,7 @@ import { checkSessionPermission } from '@/lib/permissions';
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY || process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
